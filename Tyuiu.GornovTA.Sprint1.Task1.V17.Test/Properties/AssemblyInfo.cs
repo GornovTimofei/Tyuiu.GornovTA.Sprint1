7ede826f513aa7f58f85a116c565ffe16f80cee2
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-09-21 16:47:48 +0500

    Task1

---
+++ Tyuiu.GornovTA.Sprint1.Task1.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GornovTA.Sprint1.Task1.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GornovTA.Sprint1.Task1.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ce52d44-04f0-48f2-a327-94d03e2569e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
